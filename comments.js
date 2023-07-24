fb7648b9958070bed2f90e80f5ba4a0ab73f957e
Nikhil Kumar <nk605326@gmail.com>
2023-07-24 12:52:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,27 @@
-// create web server
+// create web server with express
 var express = require('express');
-var app = express();
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var formidable = require('formidable');
 
-// create route for root
-app.get('/', function(req, res) {
-    // res.send('this is the homepage');
-    // render the homepage
-    res.render('index');
+// get the comments file
+var commentsFile = path.join(__dirname, 'comments.json');
+
+// read the comments file
+var comments = JSON.parse(fs.readFileSync(commentsFile, 'utf8'));
+
+// write the comments file
+function saveComments() {
+    fs.writeFileSync(commentsFile, JSON.stringify(comments), 'utf8');
+}
+
+// get all comments
+router.get('/comments', function(req, res) {
+    res.json(comments);
 });
 
-// create route for comments
-app.get('/comments', function(req, res) {
-    // res.send('this is the comments page');
-    res.render('comments');
-});
-
-// export this module
-module.exports = app;
+// add a new comment
+router.post('/comments', function(req, res) {
+    var newComment = {
+        id: Date.now(),
